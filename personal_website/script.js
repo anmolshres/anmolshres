cdb5a9e3cc9fc09caa09cc2965f7822fefd4e230
Anmol Shrestha <shrestha.anmol.as@gmail.com>
2020-08-08 05:37:35 -0400

    about me update

--- personal_website/script.js
+++ personal_website/script.js
@@ -12,7 +12,6 @@ let themeDots = document.getElementsByClassName('theme-dot');
 for (let i = 0; themeDots.length > i; i++) {
   themeDots[i].addEventListener('click', function () {
     let mode = this.dataset.mode;
-    console.log('Option clicked:', mode);
     setTheme(mode);
   });
 }
