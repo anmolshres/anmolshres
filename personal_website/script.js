2c0ab5bdceb8e03290b8b21b25563c3c1bc4a2ce
Anmol Shrestha <shrestha.anmol.as@gmail.com>
2020-08-08 02:33:52 -0400

    scripts update with reformat and audio pronounce

--- personal_website/script.js
+++ personal_website/script.js
@@ -1,40 +1,53 @@
-console.log('Its working')
+// put theme into local storage
+let theme = localStorage.getItem('theme');
 
-let theme = localStorage.getItem('theme')
-
-if(theme == null){
-	setTheme('light')
-}else{
-	setTheme(theme)
+if (theme == null) {
+  setTheme('light');
+} else {
+  setTheme(theme);
 }
 
-let themeDots = document.getElementsByClassName('theme-dot')
+let themeDots = document.getElementsByClassName('theme-dot');
 
-
-for (var i=0; themeDots.length > i; i++){
-	themeDots[i].addEventListener('click', function(){
-		let mode = this.dataset.mode
-		console.log('Option clicked:', mode)
-		setTheme(mode)
-	})
+for (let i = 0; themeDots.length > i; i++) {
+  themeDots[i].addEventListener('click', function () {
+    let mode = this.dataset.mode;
+    console.log('Option clicked:', mode);
+    setTheme(mode);
+  });
 }
 
-function setTheme(mode){
-	if(mode == 'light'){
-		document.getElementById('theme-style').href = 'default.css'
-	}
+function setTheme(mode) {
+  if (mode == 'light') {
+    document.getElementById('theme-style').href = 'default.css';
+  }
 
-	if(mode == 'blue'){
-		document.getElementById('theme-style').href = 'blue.css'
-	}
+  if (mode == 'blue') {
+    document.getElementById('theme-style').href = 'blue.css';
+  }
 
-	if(mode == 'green'){
-		document.getElementById('theme-style').href = 'green.css'
-	}
+  if (mode == 'green') {
+    document.getElementById('theme-style').href = 'green.css';
+  }
 
-	if(mode == 'purple'){
-		document.getElementById('theme-style').href = 'purple.css'
-	}
+  if (mode == 'purple') {
+    document.getElementById('theme-style').href = 'purple.css';
+  }
 
-	localStorage.setItem('theme', mode)
+  localStorage.setItem('theme', mode);
 }
+
+/**
+ * Name element (a tag with "Anmol") related code
+ */
+const nameElement = document.getElementById('first_name');
+nameElement.addEventListener('mouseenter', () => {
+  nameElement.innerHTML = 'Anmol🔊';
+});
+nameElement.addEventListener('mouseleave', () => {
+  nameElement.innerHTML = 'Anmol';
+});
+nameElement.addEventListener('click', () => {
+  const audioFile = new Audio('./audio/Anmol.mp3');
+  audioFile.play();
+});
